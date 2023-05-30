1fee83ddceccc2aaaf19f2746388195bbb9a2e94
Onyeriri Victor <viktorchinonso@gmail.com>
2023-05-30 10:32:31 +0100

    Created checkSpam functionality

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-let calculator = {
-    firstInput: 0,
-    secondInput: 0,
-    read() {
-        this.firstInput = +prompt('Please enter your first number: ', '');
-        this.secondInput = +prompt('Please enter your second number: ', '');
-    },
-    sum() {
-        return this.firstInput + this.secondInput;
-    },
-    multiply() {
-        return this.firstInput * this.secondInput;
-    }
+"use strict";
 
-};
+// };
 
-calculator.read();
-let total = calculator.sum;
-alert(total)
+// calculator.read();
+// let total = calculator.sum;
+// alert(total);
 
-let multiplication = calculator.multiply;
-alert(multiplication)
+// let multiplication = calculator.multiply;
+// alert(multiplication)
+
+// let ladder = {
+//     step: 0,
+//     up(){
+//         this.step++;
+//         return this;
+//     },
+//     down() {
+//         this.step--;
+//         return this
+//     },
+//     showStep() { console.log( this.step )},
+// };
+
+// console.log(ladder?.step);
+// randomNumbers(4, 5)
+
+// function randomNumbers(min, max) {
+//     console.log(Math.floor(min + Math.random() * (max+1 - min)));
+// }
+
+// const transformedWord = ucFirst('john');
+// console.log(transformedWord);
+
+// function ucFirst(name) {
+//     return name[0].toUpperCase() + 'ohn';
+// }
+
+let result = checkSpam('buy ViAgRA now');
+result = checkSpam('free xxxxx')
+result = checkSpam("innocent rabbit")
+console.log(result);
+
+function checkSpam(str) {
+    return str.toLowerCase().includes('viagra') || str.toLowerCase().includes('xxx')
+}
