f097bdd6d2fb86748e3cabe3f2b2c1fac8600069
Onyeriri Victor <viktorchinonso@gmail.com>
2023-06-02 11:42:40 +0100

    Task-1

--- script.js
+++ script.js
@@ -105,18 +105,26 @@
 //     alert(total);
 // }
 
-function getMaxSubSum(arr) {
-    let maxSum = 0;
-    let partialSum = 0;
+// function getMaxSubSum(arr) {
+//     let maxSum = 0;
+//     let partialSum = 0;
 
-    for (let item of arr) {
-        partialSum += item;
-        maxSum = Math.max(maxSum, partialSum);
-        if (partialSum < 0) partialSum = 0;
-    }
+//     for (let item of arr) {
+//         partialSum += item;
+//         maxSum = Math.max(maxSum, partialSum);
+//         if (partialSum < 0) partialSum = 0;
+//     }
 
-    return maxSum;
+//     return maxSum;
+// }
+
+// let result = getMaxSubSum([-1, -22, 3, -9, 2]);
+// console.log(result);
+
+function camelize(str) {
+    console.log(str);
+    return str.split('-').map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)).join('')
 }
 
-let result = getMaxSubSum([-1, -22, 3, -9, 2]);
+let result = camelize("-webkit-transition");
 console.log(result);
